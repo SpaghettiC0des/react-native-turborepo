d28a649023078be040fc354a353967b0d1030aa5
karlmarxlopez <7194216+karlmarxlopez@users.noreply.github.com>
2022-09-28 13:32:02 +0800

    chore: use eslint-config-react-custom

---
+++ packages/eslint-config-react-custom/index.js
@@ -0,0 +1,55 @@
+
+module.exports = {
+  extends: [
+    "plugin:import/recommended",
+    "plugin:import/typescript"
+  ],
+  "settings": {
+    "import/resolver": {
+      "typescript": {}
+    }
+  },
+  rules: {
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-sort-props": [
+      "error",
+      {
+        "ignoreCase": true,
+        "reservedFirst": true,
+        "shorthandFirst": true,
+        "callbacksLast": true
+      }
+    ],
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal"
+        ],
+        "pathGroups": [
+          {
+            "pattern": "react",
+            "group": "external",
+            "position": "before"
+          }
+        ],
+        "pathGroupsExcludedImportTypes": [
+          "react"
+        ],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
+    "import/namespace": "off",
+    "import/prefer-default-export": "off",
+    "import/export": "error",
+    "import/no-cycle": "error",
+    "import/no-anonymous-default-export": "error",
+  }
+};
